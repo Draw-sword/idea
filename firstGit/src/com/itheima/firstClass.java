1f8c9bc33768977105b3e11b05d0a39f551d3744
admin <adminitcast@git.cn>
2020-02-13 19:47:42 +0800

    把本地工程添加到本地仓库

---
+++ firstGit/src/com/itheima/firstClass.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class firstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+
+}
